531dd265c23641d4d5fc612ac4e96a019d773071
Francisco-Back <fbacks@miumg.edu.gt>
2023-05-22 23:26:41 -0600

    Inicio Proyecto

---
+++ src/main/java/com/fbacks/RegistroUser/RegistroUsuariosPorMedioDeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fbacks.RegistroUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuariosPorMedioDeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroUsuariosPorMedioDeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbacks/RegistroUser/RegistroUsuariosPorMedioDeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbacks.RegistroUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroUsuariosPorMedioDeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
